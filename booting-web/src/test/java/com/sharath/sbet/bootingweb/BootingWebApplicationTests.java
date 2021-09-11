685b959639d9c01e177d8e40dd47e3f57dd5bbb9
Sharath_Shepuri <sharathshepuri@gmail.com>
2021-09-11 11:07:16 +0800

    initial commit for spring-essential-training

---
+++ booting-web/src/test/java/com/sharath/sbet/bootingweb/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharath.sbet.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
